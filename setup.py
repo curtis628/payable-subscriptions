34ae3634b5e6af6867a939e8d493f7b38743715e
Tyler Curtis <tcurtis@vmware.com>
2023-01-02 20:45:12 -0800

    Prepare for initial PyPI release

--- setup.py
+++ setup.py
@@ -14,9 +14,19 @@ setup(
     description="Integrates out-of-the-box payment processing for django-flexible-subscriptions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=["*.tests"]),
-    package_data={ "": ["*.txt"], },
-
+    packages=find_packages(exclude=['tests*']),
+    project_urls={
+        'Source code': 'https://github.com/curtis628/payable-subscriptions',
+        'Issues': 'https://github.com/curtis628/payable-subscriptions/issues',
+    },
+    python_requires='>=3.7',
+    install_requires=[
+        'django>=4.0',
+        'django-flexible-subscriptions>=0.15.1',
+    ],
+    tests_require=[
+        'pytest>=6.0.0',
+    ],
     # See http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Framework :: Django :: 4.1',
